e5eaaa0749e34701f6d219246cf60555af7b1d44
Rafael Possenti <rafael_possenti@hotmail.com>
2018-04-13 22:06:08 -0200

    constructors and toString method

--- src/com/possenti/aopdemo/Account.java
+++ src/com/possenti/aopdemo/Account.java
@@ -4,6 +4,14 @@ public class Account {
 	private String name;
 	private String level;
 	
+	public Account() {
+	}
+	
+	public Account(String name, String level) {
+		this.name = name;
+		this.level = level;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -16,6 +24,13 @@ public class Account {
 	public void setLevel(String level) {
 		this.level = level;
 	}
+
+	@Override
+	public String toString() {
+		return "Account [name=" + name + ", level=" + level + "]";
+	}
+	
+	
 	
 	
 }
